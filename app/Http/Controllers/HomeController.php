23b77680a368e4c910800f1e43ddd551c98ada0a
Ghie Cotcheza <ghiecotcheza@msml.nl>
2023-10-25 09:26:55 +0200

    feature-add-database

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,14 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Game;
-
 class HomeController extends Controller
 {
     public function index()
     {
-        dd(Game::with(['developers', 'publisher', 'genres', 'modes', 'platforms'])->orderBy('created_at')->get());
-
         return view('index');
     }
 }
